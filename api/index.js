b2f74c8355f6572f6a6ebeae984aa9f6fceddb53
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-08-18 00:30:16 +0530

    Complete user Backend

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
 
@@ -26,8 +27,20 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
+app.use("/api/user", userRoutes);
+
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
