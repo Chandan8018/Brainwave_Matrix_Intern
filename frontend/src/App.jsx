49ace74db543d73443a1ae864c1e7dc08e7483d1
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-08-21 12:16:35 +0530

    Create About Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import ScrollToTop from "./components/ScrollToTop";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import About from "./pages/About";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
       </Routes>
       <FooterCom />
     </BrowserRouter>
