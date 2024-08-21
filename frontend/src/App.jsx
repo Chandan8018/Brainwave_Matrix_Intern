88b94cc9a365757511d3dd35220b8a4c69c58ede
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-08-21 12:10:55 +0530

    Completed SignUp and OAuth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import FooterCom from "./components/FooterCom";
 import Header from "./components/Header";
+import ScrollToTop from "./components/ScrollToTop";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
@@ -10,6 +13,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
       </Routes>
       <FooterCom />
     </BrowserRouter>
