c43ab762a39a2be47dd970abb60f5a9b33837059
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-08-22 09:01:32 +0530

    add sparkles in underline

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
+import SparklesCore from "../components/ui/sparkles";
+import { useSelector } from "react-redux";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
+  const { theme } = useSelector((state) => state.theme);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -17,7 +20,28 @@ export default function Home() {
   return (
     <div>
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
-        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+        <div className='h-[8rem] w-full bg-transparent flex flex-col items-center justify-center overflow-hidden rounded-md'>
+          <h1 className='md:text-3xl text-2xl lg:text-5xl font-bold text-center text-black dark:text-white relative z-20'>
+            Welcome to my Blog
+          </h1>
+          <div className='w-[40rem] h-40 relative'>
+            {/* Gradients */}
+            <div className='absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm' />
+            <div className='absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4' />
+            <div className='absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm' />
+            <div className='absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4' />
+
+            {/* Core component */}
+            <SparklesCore
+              background='transparent'
+              minSize={0.4}
+              maxSize={1}
+              particleDensity={1200}
+              className='w-96 h-10 ml-32'
+              particleColor={theme === "dark" ? "#FFFFFF" : "#00050C"}
+            />
+          </div>
+        </div>
         <p className='text-gray-500 text-xs sm:text-sm'>
           Here you'll find a variety of articles and tutorials on topics such as
           web development, software engineering, and programming languages.
@@ -36,7 +60,28 @@ export default function Home() {
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
-            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='h-[8rem] w-full bg-transparent flex flex-col items-center justify-center overflow-hidden rounded-md'>
+              <h1 className='md:text-4xl text-2xl lg:text-7xl font-bold text-center text-black dark:text-white relative z-20'>
+                Recent Posts
+              </h1>
+              <div className='w-[40rem] h-40 relative'>
+                {/* Gradients */}
+                <div className='absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm' />
+                <div className='absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4' />
+                <div className='absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm' />
+                <div className='absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4' />
+
+                {/* Core component */}
+                <SparklesCore
+                  background='transparent'
+                  minSize={0.4}
+                  maxSize={1}
+                  particleDensity={1200}
+                  className='w-96 h-10 ml-32'
+                  particleColor={theme === "dark" ? "#FFFFFF" : "#070801"}
+                />
+              </div>
+            </div>
             <div className='flex flex-wrap gap-4'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
--- frontend/src/pages/UpdatePost.jsx
+++ frontend/src/pages/UpdatePost.jsx
@@ -136,6 +136,8 @@ export default function UpdatePost() {
             <option value='javascript'>JavaScript</option>
             <option value='reactjs'>React.js</option>
             <option value='nextjs'>Next.js</option>
+            <option value='Health'>Health</option>
+            <option value='Others'>Others</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
