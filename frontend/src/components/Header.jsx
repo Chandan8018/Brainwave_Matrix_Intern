d04f5923519e9b604c7769df458e000d1608ba44
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-10-05 06:57:24 +0530

    Some modification on header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -90,7 +90,7 @@ export default function Header() {
     <Navbar className='border-b-2'>
       <Link
         to='/'
-        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white md:pl-7'
       >
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
           ThinkBig's
